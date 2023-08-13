903c0abd260c0b89afc64a48380b4a5cd428bfd4
Syed Faraz Haider <farazhaider88@yahoo.com>
2023-08-14 02:02:42 +0500

    fix errors

--- NyTimesMostPopularArticles/View/ArticleListViewController.swift
+++ NyTimesMostPopularArticles/View/ArticleListViewController.swift
@@ -7,7 +7,6 @@
 
 import UIKit
 import Combine
-import NVActivityIndicatorView
 
 class ArticleListViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
